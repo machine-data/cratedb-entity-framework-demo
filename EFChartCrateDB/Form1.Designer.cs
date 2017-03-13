3b2726c441b11051cdf00526860ab08355d96fcb
Michael Beer <michael@crate.io>
2017-03-13 15:19:39 +0100

    initial commit

---
+++ EFChartCrateDB/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace EFChartCrateDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.sensorChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sensorChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sensorChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.sensorChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.sensorChart.Legends.Add(legend1);
+            this.sensorChart.Location = new System.Drawing.Point(32, 37);
+            this.sensorChart.Name = "sensorChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "temp";
+            this.sensorChart.Series.Add(series1);
+            this.sensorChart.Size = new System.Drawing.Size(1444, 653);
+            this.sensorChart.TabIndex = 0;
+            this.sensorChart.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sensor Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1488, 716);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sensorChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sensorChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart sensorChart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EFChartCrateDB/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Windows.Forms;
+using System;
+
+namespace EFChartCrateDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            using (var db = new CrateDBContext())
+            {
+                // display all posts from CrateDB
+                var query = (from s in db.Data
+                            orderby s.ts ascending
+                            group s by s.ts into g
+                            select new {
+                                ts = g.Key,
+                                max_value = g.Average(s => s.value)
+                            }).Take(50);
+
+
+                foreach (var item in query)
+                {
+                    DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                    dtDateTime = dtDateTime.AddMilliseconds(item.ts);
+
+                    sensorChart.Series["temp"].Points.AddXY(dtDateTime.ToString("hh:mm:ss.fff"), item.max_value);
+                }
+            }
+
+        }
+    }
+
+    [Table("doc.sensor_data")]
+    public class SensorData
+    {
+        [Key]
+        public string id { get; set; }
+        public float value { get; set; }
+        public long ts { get; set; }
+    }
+
+    public class CrateDBContext : DbContext
+    {
+        public CrateDBContext()
+        {
+            //Configuration.AutoDetectChangesEnabled = false;
+            Database.SetInitializer<CrateDBContext>(null);
+        }
+
+        public DbSet<SensorData> Data { get; set; }
+    }
+
+}
